636025b3d80a1858a8a29166742c15ee68c444ac
divya_singh <divya_singh@gmail.com>
2025-04-12 19:06:48 +0000

    converted into web-server

--- reverse_string.go
+++ reverse_string.go
@@ -1,20 +1,22 @@
 package main 
-import "fmt"
-func main(){
-fmt.Print("enter your string : ")
-var str string 
-fmt.Scanln(&str)
-fmt.Print("\n")
-reverse(str)
-fmt.Println("thank you for using our app")
-
-
+import {"fmt"
+"net/http"
+"log"
 }
-func reverse (str string ){
+func reverse (str string ) string{
 var ans = []byte{}
 for j := len(str)-1 ; j>=0 ; j--{
 ans = append(ans , str[j])
 }
 fmt.Println(string(ans))
-
+return string(ans)
 }
+func response_sender_function(w http.ResponseWriter , r *http.Request){
+fmt.Fprintf(w , reverse(r.FormValue("text"))
+}
+func main(){
+http.Handlefunc("/reverse" , response_sender_function)
+log.Println("this server is running at http://localhost:8080")
+log.Fatal(http.ListenAndServe("8080" , nil)
+}
+
