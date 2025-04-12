d129ba92f2e3b23a735a8803f566268e86558c4a
divya_singh <divya_singh@gmail.com>
2025-04-12 21:09:14 +0000

    server now working-previous commit was faulted

--- reverse_string.go
+++ reverse_string.go
@@ -1,22 +1,21 @@
 package main 
-import {"fmt"
+import ("fmt"
 "net/http"
 "log"
-}
+)
 func reverse (str string ) string{
 var ans = []byte{}
 for j := len(str)-1 ; j>=0 ; j--{
 ans = append(ans , str[j])
 }
-fmt.Println(string(ans))
 return string(ans)
 }
 func response_sender_function(w http.ResponseWriter , r *http.Request){
-fmt.Fprintf(w , reverse(r.FormValue("text"))
+fmt.Fprintf(w , reverse(r.FormValue("text")))
 }
 func main(){
-http.Handlefunc("/reverse" , response_sender_function)
+http.HandleFunc("/reverse" , response_sender_function)
 log.Println("this server is running at http://localhost:8080")
-log.Fatal(http.ListenAndServe("8080" , nil)
+log.Fatal(http.ListenAndServe(":8080" , nil))
 }
 
