af94457882f8efc9a768c7e37da248a553875da6
divya_singh <divya_singh@gmail.com>
2025-04-14 19:02:10 +0000

    now the database is properly working

--- db.go
+++ db.go
@@ -5,12 +5,13 @@ import(
 _ "github.com/lib/pq"
 )
 func connect(input string , output string) error{
-db, err :=sql.Open("postgres","host=localhost port=5432 user=myself dbname=requests sslmode=disable")
+db, err :=sql.Open("postgres","host=localhost port=5432 user=postgres dbname=requests sslmode=disable")
 if err != nil{
 return fmt.Errorf("error in accesing the database : %v",err)
 }
 defer db.Close()
-_, err = db.Exec("INSERT INTO requests (input , output) VALUES ($1 ,$2)",input, output)
+if err :=db.Ping();err!=nil{fmt.Errorf("error while connecting to database" , err)}
+_, err = db.Exec("INSERT INTO reversed_strings (input , output) VALUES ($1 ,$2)",input, output)
 if err != nil{
 return fmt.Errorf("error in inserting the values : %v" ,err)
 }
