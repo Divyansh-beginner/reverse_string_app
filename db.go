68f5db8ff98dc2c43b27041d9f231b87a27b11f4
divya_singh <divya_singh@gmail.com>
2025-04-22 15:31:22 +0000

    added the password in compose file and db.go file for psql container , and previous volume was removed and remade for the password one

--- db.go
+++ db.go
@@ -5,7 +5,7 @@ import(
 _ "github.com/lib/pq"
 )
 func connect(input string , output string) error{
-db, err :=sql.Open("postgres","host=pg_service3 port=5432 user=postgres dbname=requestsdb_docker sslmode=disable")
+db, err :=sql.Open("postgres","host=pg_service3 port=5432 user=postgres password=987654321 dbname=requestsdb_docker sslmode=disable")
 
 if err != nil{
 return fmt.Errorf("error in accesing the database : %v",err)
